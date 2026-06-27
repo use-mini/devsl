22bdd3d5494768dbf6b25f528f2dff9c82d0d507
vkhashimoto <git@knj.email>
2026-06-27 08:31:54 -0300

    chore: init repository and project

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A dev REPL";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-26.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.nixfmt
+
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.rustfmt
+            pkgs.clippy
+            pkgs.rust-analyzer
+
+            pkgs.cocogitto
+          ];
+        };
+      }
+    );
+}
